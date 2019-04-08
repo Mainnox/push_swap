1499369278e01af47d6cc1a2ebe46c7fc55d9fac
Arnaud KREMER <akremer@e2r8p1.42.fr>
2019-04-08 12:59:02 +0200

    Sauvegarde auto !

--- srcs_push_swap/ft_sort_push_swap.c
+++ srcs_push_swap/ft_sort_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: akremer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 06:56:54 by akremer           #+#    #+#             */
-/*   Updated: 2019/04/08 12:12:37 by akremer          ###   ########.fr       */
+/*   Updated: 2019/04/08 12:44:42 by akremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void			ft_wich_path(t_push *handle, int where, void (*f)(t_push *handle), void (
 {
 	int n;
 
-//	ft_printf("where = %d\n", where);
+	//	ft_printf("where = %d\n", where);
 	if (where > handle->sizea / 2)
 	{
 		n = handle->sizea - where - 1;
@@ -90,8 +90,8 @@ static void			ft_wich_path2(t_push *handle, int nb, void (*f)(t_push *handle), v
 	int where;
 
 	where = ft_find_this(handle->b, handle->sizeb, nb);
-//	ft_printf("next = %d\n", nb);
-//	ft_printf("where = %d\n", where);
+	//	ft_printf("next = %d\n", nb);
+	//	ft_printf("where = %d\n", where);
 	if (where > handle->sizeb / 2)
 	{
 		while (ft_find_this(handle->b, handle->sizeb, nb) != pole)
@@ -102,8 +102,8 @@ static void			ft_wich_path2(t_push *handle, int nb, void (*f)(t_push *handle), v
 		while (ft_find_this(handle->b, handle->sizeb, nb) != pole)
 			ft(handle);
 	}
-//	ft_print_tab(handle->a, handle->sizea, "handle->a");
-//	ft_print_tab(handle->b, handle->sizeb, "handle->b");
+	//	ft_print_tab(handle->a, handle->sizea, "handle->a");
+	//	ft_print_tab(handle->b, handle->sizeb, "handle->b");
 }
 
 static void			ft_while_f(t_push *handle, int n, void (*f)(t_push *handle), void(*ft)(t_push *handle))
@@ -255,45 +255,28 @@ void				ft_algo_insert_b(t_push *handle)
 	max = ft_find_bigger(handle->b, handle->sizeb);
 	i = 2;
 	j = 0;
-//	ft_printf("Max avant la boucle = %d\n", max);
+	//	ft_printf("Max avant la boucle = %d\n", max);
 	while (i <= handle->sizeb)
 	{
-		if (handle->b[0] == max)
-		{
-			ft_push_b(handle);
-			max = ft_find_n_bigger(handle->b, handle->sizeb, max);
-			if (i != 2)
-				i--;
-			rotate++;
-			continue ;
-		}
-		if (handle->b[0] == handle->low)
-		{
-			ft_put_b_on_a(handle, 1);
-			handle->low = ft_find_low(handle);
-			if (i != 2)
-				i--;
-			continue ;
-		}
 		if (!ft_is_nsort(handle->b, i))
 		{
-		if (handle->b[0] == max)
-		{
-			ft_push_b(handle);
-			max = ft_find_n_bigger(handle->b, handle->sizeb, max);
-			if (i != 2)
-				i--;
-			rotate++;
-			continue ;
-		}
-		if (handle->b[0] == handle->low)
-		{
-			ft_put_b_on_a(handle, 1);
-			handle->low = ft_find_low(handle);
-			if (i != 2)
-				i--;
-			continue ;
-		}
+			if (handle->b[0] == max)
+			{
+				ft_push_b(handle);
+				max = ft_find_n_bigger(handle->b, handle->sizeb, max);
+				if (i != 2)
+					i--;
+				rotate++;
+				continue ;
+			}
+			if (handle->b[0] == handle->low)
+			{
+				ft_put_b_on_a(handle, 1);
+				handle->low = ft_find_low(handle);
+				if (i != 2)
+					i--;
+				continue ;
+			}
 			if (handle->b[0] > handle->b[1])
 				ft_swap_b(handle);
 			if (!ft_is_nsort(handle->b, i))
@@ -326,80 +309,80 @@ void				ft_algo_insert_b(t_push *handle)
 	}
 }
 /*
-static t_push			*ft_make_save(t_push *handle)
-{
-	t_push		*save;
+   static t_push			*ft_make_save(t_push *handle)
+   {
+   t_push		*save;
 
-	if (!(save = (t_push*)malloc(sizeof(t_push))))
-		ft_print_error();
-	if (!(save->a = (int*)malloc(sizeof(int) * handle->size)))
-			ft_print_error();
-	if (!(save->b = (int*)malloc(sizeof(int) * handle->size)))
-		ft_print_error();
-	if (!(save->hack = (char*)malloc(sizeof(char) * 1)))
-		ft_print_error();
-	ft_bzero((void*)save->b, handle->size);
-	save->sizea = handle->sizea;
-	save->sizeb = handle->sizeb;
-	save->nb_ope = 0;
-	save->a = ft_memcpy((void*)save->a, (void*)handle->a, handle->sizea);
-	save->b = ft_memcpy((void*)save->b, (void*)handle->b, handle->sizeb);
-	return (save);
-}
+   if (!(save = (t_push*)malloc(sizeof(t_push))))
+   ft_print_error();
+   if (!(save->a = (int*)malloc(sizeof(int) * handle->size)))
+   ft_print_error();
+   if (!(save->b = (int*)malloc(sizeof(int) * handle->size)))
+   ft_print_error();
+   if (!(save->hack = (char*)malloc(sizeof(char) * 1)))
+   ft_print_error();
+   ft_bzero((void*)save->b, handle->size);
+   save->sizea = handle->sizea;
+   save->sizeb = handle->sizeb;
+   save->nb_ope = 0;
+   save->a = ft_memcpy((void*)save->a, (void*)handle->a, handle->sizea);
+   save->b = ft_memcpy((void*)save->b, (void*)handle->b, handle->sizeb);
+   return (save);
+   }
 
-static void				ft_sort_a(t_push *handle)
-{
+   static void				ft_sort_a(t_push *handle)
+   {
 //	t_push		*save;
 
 //	save = ft_make_save(handle);
-	return ;
+return ;
 }
 
 static int				ft_find_mid(int *tab, int size, int ign)
 {
-	int		*save;
-	int		to_find;
-	int		i;
+int		*save;
+int		to_find;
+int		i;
 
-	size -= ign;
-	i = 0;
-	to_find = 0;
-	if (!(save = (int*)malloc(sizeof(int) * size)))
-		ft_print_error();
-	save = ft_memcpy(save, tab, size);
-	ft_sort_integer_table(save, size);
-	while (i < size / 2)
-		i++;
-	to_find = save[i];
-	free(save);
-	return (to_find);
+size -= ign;
+i = 0;
+to_find = 0;
+if (!(save = (int*)malloc(sizeof(int) * size)))
+ft_print_error();
+save = ft_memcpy(save, tab, size);
+ft_sort_integer_table(save, size);
+while (i < size / 2)
+i++;
+to_find = save[i];
+free(save);
+return (to_find);
 }
 
 static void				ft_reduc_a_2(t_push *handle)
 {
-	int		median;
-	int		i;
-	int		j;
+int		median;
+int		i;
+int		j;
 
-	j = 0;
-	i = 0;
-	median = ft_find_mid(handle->a, handle->sizea, handle->ign);
-	while (i < (handle->sizea - handle->ign))
- 	{
-		if (handle->a[0] < median)
-			ft_push_a(handle);
-		else
-		{
-			ft_rotate_a(handle);
-			j++;
-		}
-		i++;
-	}
-	while (j)
-	{
-		ft_reverse_rotate_a(handle);
-		j--;
-	}
+j = 0;
+i = 0;
+median = ft_find_mid(handle->a, handle->sizea, handle->ign);
+while (i < (handle->sizea - handle->ign))
+{
+if (handle->a[0] < median)
+ft_push_a(handle);
+else
+{
+ft_rotate_a(handle);
+j++;
+}
+i++;
+}
+while (j)
+{
+	ft_reverse_rotate_a(handle);
+	j--;
+}
 }
 
 static void				ft_reduc_b_2(t_push *handle)
@@ -412,7 +395,7 @@ static void				ft_reduc_b_2(t_push *handle)
 	i = 0;
 	median = ft_find_mid(handle->b, handle->sizeb, handle->ign);
 	while (i < (handle->sizeb - handle->ign))
- 	{
+	{
 		if (handle->b[0] < median)
 			ft_push_b(handle);
 		else
@@ -444,7 +427,7 @@ static void				ft_reduc_a(t_push *handle)
 	else
 		median = ft_find_mid(handle->a, handle->sizea, handle->ign);
 	while (i < size)
- 	{
+	{
 		if (handle->a[0] < median)
 			ft_push_a(handle);
 		else
@@ -588,6 +571,6 @@ void					ft_sort_push_swap(t_push *handle, int algo_pass)
 		ft_algo_perso_1(handle);
 	if (algo_pass == 2)
 		ft_algo_insert_a(handle);
-//	ft_print_tab(handle->a, handle->sizea, "handle->a");
-//	ft_print_tab(handle->b, handle->sizeb, "handle->b");
+	//	ft_print_tab(handle->a, handle->sizea, "handle->a");
+	//	ft_print_tab(handle->b, handle->sizeb, "handle->b");
 }
